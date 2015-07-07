9a37e98ff436da0c588af57a74e833ce9e23fdc2
Clinton Weiss <cvweiss@gmail.com>
2015-07-07 22:00:29 +0000

    action tests added

---
+++ test/test_action_listen.php
@@ -0,0 +1,5 @@
+<?php
+
+require_once "../init.php";
+
+print_r(RedisQ\Action::listen());
---
+++ test/test_action_send.php
@@ -0,0 +1,5 @@
+<?php
+
+require_once "../init.php";
+
+RedisQ\Action::queue(['package' => ['foo' => 'bar', 'abc' => 'def']]);
