c454711378a0a1f61beb971cc079027a5e4e0525
Clinton Weiss <cvweiss@gmail.com>
2015-07-07 17:28:01 +0000

    a basic test 'unit'

---
+++ test/QnA.php
@@ -0,0 +1,38 @@
+<?php
+
+require_once "../init.php";
+
+// First, register as a listener
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, 'redisq.zkillboard.com/listen.php');
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, "queueID=TestQ");
+$response = curl_exec($ch);
+
+echo "Initial listen may return a null package\n";
+print_r(json_decode($response,true));
+echo "\n";
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, 'redisq.zkillboard.com/queue.php');
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_POST, 1);
+$auth = $queueAuth[0];
+$user = (string) key($auth);
+$pass = $auth[$user];
+curl_setopt($ch, CURLOPT_POSTFIELDS, "user=$user&pass=$pass&package=1234");
+echo "Queueing 1234 as a package...\n";
+$response = curl_exec($ch);
+echo "Package queued...\n";
+print_r(json_decode($response,true));
+
+echo "Now listen for the package we just queued\n";
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, 'redisq.zkillboard.com/listen.php');
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, "queueID=TestQ");
+$response = curl_exec($ch);
+print_r(json_decode($response,true));
+echo "Success!\n";
