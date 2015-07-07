b7415a30053ace7b19d833777fcfa030842cf4dd
Clinton Weiss <cvweiss@gmail.com>
2015-07-07 15:16:27 +0000

    proper json output

--- public/listen.php
+++ public/listen.php
@@ -5,7 +5,7 @@ require_once '../init.php';
 header('Content-Type: application/json');
 if (@$_SERVER['REQUEST_METHOD'] !== 'POST') {
     header('HTTP/1.1 405 Method not allowed');
-    echo "['error':'Method not allowed, only use POST']";
+    echo '{"error":"Method not allowed, only use POST"}';
     exit();
 }
 
--- public/queue.php
+++ public/queue.php
@@ -5,12 +5,30 @@ require_once '../init.php';
 header('Content-Type: application/json');
 if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
     header('HTTP/1.1 405 Method not allowed');
-    echo "['error':'Method not allowed, only use POST']";
+    echo '{"error":"Method not allowed, only use POST"}';
     exit();
 }
 
-$authUser = $_POST['user'];
-$authPass = $_POST['pass'];
-$package = $_POST['package'];
+$authUser = @$_POST['user'];
+$authPass = @$_POST['pass'];
+$package = @$_POST['package'];
+
+$authed = false;
+foreach ($queueAuth as $user=>$pass) {
+	if ($authUser == $user && $authPass == $pass) $authed |= true;
+	if ($authed) break;
+}
+
+if ($authed === false) {
+    header('HTTP/1.1 401 Unauthorized');
+    echo '{"error":"Unauthorized"}';
+    exit();
+}
+
+if ($package == null) {
+    header('HTTP/1.1 400 Bad Data');
+    echo '{"error":"Bad Data: package was empty"}';
+    exit();
+}
 
 RedisQ\RedisQ::queueObject($package);
