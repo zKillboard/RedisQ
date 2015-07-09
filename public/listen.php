ce4ecea2e00c1adf35c40886164680f9e3c46893
root <root@ip-172-31-46-168.ec2.internal>
2015-07-09 23:15:24 +0000

    Removed requirement for POST

--- public/listen.php
+++ public/listen.php
@@ -2,13 +2,6 @@
 
 require_once '../init.php';
 
-header('Content-Type: application/json');
-if (@$_SERVER['REQUEST_METHOD'] !== 'POST') {
-    header('HTTP/1.1 405 Method not allowed');
-    echo '{"error":"Method not allowed, only use POST"}';
-    exit();
-}
-
 $queueID = @$_POST['queueID'];
 if ($queueID === null) {
     $queueID = RedisQ\IP::get();
