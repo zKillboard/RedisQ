277d916cd326845ea6bf0dfc2ef53f3091d6d26f
Clinton Weiss <cvweiss@gmail.com>
2016-03-26 01:41:38 +0000

    added cors stuff

--- public/listen.php
+++ public/listen.php
@@ -11,5 +11,7 @@ $package = unserialize(RedisQ\RedisQ::listen($queueID));
 if ($package === false) $package = null;
 
 $response = ['package' => $package];
+header("Access-Control-Allow-Origin: *");
+header('Access-Control-Allow-Methods: GET');
 header('Content-Type: application/json');
 echo json_encode($response);
