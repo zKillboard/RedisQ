85832e3f623d4e6b5fa36f1a9500cb07af3fd049
Clinton Weiss <cvweiss@gmail.com>
2016-12-27 02:28:25 +0000

    Fixes #5

--- public/listen.php
+++ public/listen.php
@@ -4,6 +4,9 @@ require_once '../init.php';
 
 $queueID = @$_POST['queueID'];
 if ($queueID === null) {
+    $queueID = @$_GET['queueID'];
+}
+if ($queueID === null) {
     $queueID = RedisQ\IP::get();
 }
 
