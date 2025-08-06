f760a87086f854e22e09cbaa7ff42f1b3e0842ab
cvweiss <cvweiss@gmail.com>
2025-08-06 23:36:24 +0000

    various changes including rate limitations

--- www/controllers/listen.js
+++ www/controllers/listen.js
@@ -1,38 +1,48 @@
 'use strict';
 
 module.exports = {
-	paths: '/listen.php',
-	get: get
+    paths: '/listen.php',
+    get: get
 }
 
 async function get(req, res, app) {
-	try {
-		const queueID = (req.query.queueID || '').trim();
-		const ttw = Math.min(10, Math.max(1, parseInt(req.query.ttw || 10)));
-		let sackID, t = 0;
+    const queueID = (req.query.queueID || '').trim();
+    const lockKey = `redisQ:lock:${queueID}`;
+    let lockAcquired = false;
+    try {
+        lockAcquired =  await app.redis.set(lockKey, "1", "NX", "EX", 30);
+        if (lockAcquired != 'OK') {
+            return {status_code: 429};
+        }
+
+        const ttw = Math.min(10, Math.max(1, parseInt(req.query.ttw || 10)));
+        let sackID, t = 0;
 
         if (queueID.length > 0) {
-		    await app.redis.setex('redisQ:queue:' + queueID, 9600, ".");
+            await app.redis.setex('redisQ:queue:' + queueID, 9600, ".");
         }
-		do {
+        do {
             let repeat = false;
             let count = 0;
+            // really need to switch to ioredis here
             do {
-			    sackID = await app.redis.rpop('redisQ:list:' + queueID);
+                sackID = await app.redis.rpop('redisQ:list:' + queueID);
                 count++;
             } while (sackID != null && await app.redis.get(sackID) == null && count < 25000);
-			if (sackID == null) {
-				const start = Date.now();
-				do { await app.sleep(100); } while ((Date.now() - start) < 1000);
-				t = t + 1; 
-			}
-		} while (sackID == null && t <= ttw);
-		const sack = sackID == null ? null : JSON.parse(await app.redis.get(sackID));
+            if (sackID == null) {
+                const start = Date.now();
+                do { await app.sleep(100); } while ((Date.now() - start) < 1000);
+                t = t + 1; 
+            }
+        } while (sackID == null && t <= ttw);
+        const sack = sackID == null ? null : JSON.parse(await app.redis.get(sackID));
 
         if (queueID.length == 0) return {status_code: 400};
-		return {json: {package: sack}, 'cors': '*'};
-	} catch (e) {
-		console.error(e);
-		return {status_code: 503};
-	}
+        return {json: {package: sack}, 'cors': '*'};
+    } catch (e) {
+        console.error(e);
+        return {status_code: 503};
+    } finally {
+        if (lockAcquired) await app.redis.del(lockKey);
+    }
 }
