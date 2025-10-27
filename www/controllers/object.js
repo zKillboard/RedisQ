a673a313518c50f4d3011951f37d418649332fb3
cvweiss <cvweiss@gmail.com>
2025-10-27 11:28:35 +0000

    ensure cache time is set

--- www/controllers/object.js
+++ www/controllers/object.js
@@ -5,6 +5,8 @@ module.exports = {
     get: get
 }
 
+const default_ttl = parseInt(process.env.default_ttl || 10800);
+
 async function get(req, res, app) {
     const objectID = (req.query.objectID || '').trim();
     if (objectID.length == 0) return {status_code: 404};
@@ -17,9 +19,10 @@ async function get(req, res, app) {
         }
 
         let o = JSON.parse( object );
-        return { json: { package: o }, 'cors': '*' };
+        return { json: { package: o }, ttl: default_ttl, 'cors': '*' };
     } catch (e) {
         console.error(e);
         return {status_code: 503};
     }
 }
+
